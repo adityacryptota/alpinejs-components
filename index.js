6a4c096b9c6ec676af513eb8bb06b10dcf01046d
adityacryptota <adityacryptota@gmail.com>
2022-07-05 03:19:29 +0530

    Code cleanup and code organization.

---
+++ index.js
@@ -0,0 +1,19 @@
+// AlpineJS Model
+function alpinejsModel() {
+  return {
+    title: "Model title",
+    desc: "This is model description",
+    buttons: [
+      {
+        text: 'Purchase',
+        type: 'primary'
+      },
+      {
+        text: 'Cancel',
+        type: 'secondary'
+      },
+    ]
+  }
+}
+
+// End of AlpineJS Model
